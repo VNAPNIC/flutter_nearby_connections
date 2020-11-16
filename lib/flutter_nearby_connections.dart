5e1aa8098b7003bf9550367f41bc34ef57220a91
Nankai <nam.bui@vmodev.com>
2020-11-16 15:26:16 +0700

    fix lint

--- lib/flutter_nearby_connections.dart
+++ lib/flutter_nearby_connections.dart
@@ -2,7 +2,6 @@ library flutter_nearby_connections;
 
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
