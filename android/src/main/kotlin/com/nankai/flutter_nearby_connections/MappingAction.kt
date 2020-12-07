032b11f34e197e8cd69314cc3b156f1b5985ea95
vnapnic <vn.apnic@gmail.com>
2020-12-07 17:42:43 +0700

    check bluetooth on/off check bluetooth exist on devices

--- android/src/main/kotlin/com/nankai/flutter_nearby_connections/MappingAction.kt
+++ android/src/main/kotlin/com/nankai/flutter_nearby_connections/MappingAction.kt
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.util.Log
 import com.google.android.gms.nearby.connection.AdvertisingOptions
 import com.google.android.gms.nearby.connection.DiscoveryOptions
@@ -18,12 +19,17 @@ class MappingAction(private val service: NearbyService) {
 
     private val TAG = "MappingAction"
 
+    private var state = BluetoothAdapter.STATE_OFF
+
+    private var bluetoothExist = false
+
     private lateinit var nearByConnectApiUtils: NearByConnectApiUtils
     private lateinit var wifiP2pUtils: WifiP2PUtils
 
     private val bluetoothChangeReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent) {
             val action = intent.action
+
             if (action == BluetoothAdapter.ACTION_STATE_CHANGED) {
                 val state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
                         BluetoothAdapter.ERROR)
@@ -37,14 +43,38 @@ class MappingAction(private val service: NearbyService) {
         }
     }
 
-    fun create(){
-        val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
-        service.registerReceiver(bluetoothChangeReceiver, filter)
+    private fun logBluetooth() {
+        Log.i(TAG, when (state) {
+            BluetoothAdapter.STATE_OFF -> "Bluetooth off"
+            BluetoothAdapter.STATE_TURNING_OFF -> "Turning Bluetooth off..."
+            BluetoothAdapter.STATE_ON -> "Bluetooth on"
+            BluetoothAdapter.STATE_TURNING_ON -> "Turning Bluetooth on..."
+            else -> "Bluetooth off"
+        })
     }
 
-   fun initNearBy( channel: MethodChannel,  serviceType: String ){
-       nearByConnectApiUtils = NearByConnectApiUtils(channel, serviceType, service)
-       wifiP2pUtils = WifiP2PUtils(channel, serviceType, service)
+    fun create() {
+        val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        service.registerReceiver(bluetoothChangeReceiver, filter)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            bluetoothExist = service.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+        }
+
+        if (!bluetoothExist) {
+            bluetoothExist = service.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)
+        }
+
+        Log.i(TAG, "Bluetooth exist on the device is $bluetoothExist")
+
+        if (bluetoothExist) {
+            setBluetooth(false)
+        }
+    }
+
+    fun initNearBy(channel: MethodChannel, serviceType: String) {
+        nearByConnectApiUtils = NearByConnectApiUtils(channel, serviceType, service)
+        wifiP2pUtils = WifiP2PUtils(channel, serviceType, service)
     }
 
 
@@ -86,6 +116,18 @@ class MappingAction(private val service: NearbyService) {
         stopAdvertising()
         stopDiscovery()
         nearByConnectApiUtils.stopAllEndpoints()
+    }
+
+    private fun setBluetooth(enable: Boolean): Boolean {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val isEnabled = bluetoothAdapter.isEnabled
+        if (enable && !isEnabled) {
+            return bluetoothAdapter.enable()
+        } else if (!enable && isEnabled) {
+            return bluetoothAdapter.disable()
+        }
+        // No need to change bluetooth state
+        return true
     }
 
 }
