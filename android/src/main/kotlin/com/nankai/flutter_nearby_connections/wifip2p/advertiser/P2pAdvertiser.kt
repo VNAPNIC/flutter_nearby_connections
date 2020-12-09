ecb401eee4625f1dbfb6477afa7b6ed2121b0157
vnapnic <vn.apnic@gmail.com>
2020-12-09 10:15:35 +0700

    remove class P2pAdvertiser.kt and P2pDiscovery.kt

--- android/src/main/kotlin/com/nankai/flutter_nearby_connections/wifip2p/advertiser/P2pAdvertiser.kt
+++
@@ -1,4 +0,0 @@
-package com.nankai.flutter_nearby_connections.wifip2p.advertiser
-
-class P2pAdvertiser {
-}
--- android/src/main/kotlin/com/nankai/flutter_nearby_connections/wifip2p/discovery/P2pDiscovery.kt
+++
@@ -1,4 +0,0 @@
-package com.nankai.flutter_nearby_connections.wifip2p.discovery
-
-class P2pDiscovery {
-}
